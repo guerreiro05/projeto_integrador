790abe9d62a4d5e1ed84e7ff436a39f38c3b75f0
Gabriel Dias <gabesarcal215@gmail.com>
2025-01-29 21:34:27 -0300

    Criando menu, navegacao, e rodape

---
+++ app/components/Navegacao.js
@@ -0,0 +1,19 @@
+export default Navegacao
+
+function Navegacao() {
+    return ( 
+    <main>
+        <nav>
+            <ul className="w-80 bg-red-300 flex justify-between">
+                <li>Categorias</li>
+                <li>Ofertas</li>
+                <li>Planos e Pacotes</li>
+                <li>Eventos e Torneios</li>
+                <li>Primeira Partida Grátis</li>
+                <li>Anuncie sua Quadra</li>
+                <li>Ajuda e Suporte</li>
+            </ul>
+        </nav>
+    </main>
+    );
+}
---
+++ app/components/Rodape.js
@@ -0,0 +1,35 @@
+export default RodaPe;
+function RodaPe() {
+    return ( 
+    <main>
+        <div className="rodaPe">
+            <div>
+                <a href="">R. Episcopal, 700 - Centro, São Carlos - SP, 13560-570</a>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d946853.9665162596!2d-48.55058348749999!3d-22.024782599999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b87722afe006bb%3A0x4a8b254e7543696!2sSenac%20S%C3%A3o%20Carlos!5e0!3m2!1spt-BR!2sbr!4v1733445699924!5m2!1spt-BR!2sbr" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div>
+                <h3>Sobre nós</h3>
+                <p>Teste</p>
+                <p>Teste</p>
+                <p>Teste</p>
+            </div>
+            <div>
+                <h3>Entre em contato</h3>
+                <p>Teste</p>
+                <p>Teste</p>
+                <p>Teste</p>
+            </div>
+            <div>
+                <h3>Entre em contato</h3>
+                <p>Teste</p>
+                <p>Teste</p>
+                <p>Teste</p>
+            </div>
+        </div>
+
+        <hr/>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nam recusandae in, voluptate nihil minima earum, dignissimos fugit sapiente fuga modi rerum maxime qui quam sint reprehenderit cum ducimus ipsa.</p>
+    </main>
+     );
+}
