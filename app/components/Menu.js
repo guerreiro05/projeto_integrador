80869f204186b04e7d55495f85b3b0c5e6e7da4d
Gabriel Dias <gabesarcal215@gmail.com>
2025-01-29 22:11:16 -0300

    Adicionando imagens e separando o HTML em componentes

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -3,23 +3,21 @@ export default Menu;
 function Menu() {
     return ( 
     <main>
-        <div className="cabecalho">
+        <div className="w-100 bg-red-300 flex justify-between">
+            <div>
+                <img className="" width="100" src="./imagens/logo.jpg"/>
+            </div>
 
-        <div>
-        <img className="" width="100" src="./imagens/logo.jpg"/>
-        </div>
+            <div className="pesquisar">
+                <i className="fa-solid fa-magnifying-glass"></i>
+                <input type="text"/>
+            </div>
 
-        <div className="pesquisar">
-            <i className="fa-solid fa-magnifying-glass"></i>
-            <input type="text"/>
-        </div>
-
-        <p>
-            <button>Crie a sua conta</button>
-            <button>Entre</button>
-            <button>Calendário</button>
-        </p>
-
+            <p>
+                <button>Crie a sua conta</button>
+                <button>Entre</button>
+                <button>Calendário</button>
+            </p>
         </div> 
     </main>
     );
--- app/components/Rodape.js
+++ app/components/Rodape.js
@@ -2,28 +2,26 @@ export default RodaPe;
 function RodaPe() {
     return ( 
     <main>
-        <div className="rodaPe">
+        <div className="w-100 bg-red-300 flex justify-between">
             <div>
                 <a href="">R. Episcopal, 700 - Centro, São Carlos - SP, 13560-570</a>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d946853.9665162596!2d-48.55058348749999!3d-22.024782599999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b87722afe006bb%3A0x4a8b254e7543696!2sSenac%20S%C3%A3o%20Carlos!5e0!3m2!1spt-BR!2sbr!4v1733445699924!5m2!1spt-BR!2sbr" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <br/>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d946853.9665162596!2d-48.55058348749999!3d-22.024782599999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b87722afe006bb%3A0x4a8b254e7543696!2sSenac%20S%C3%A3o%20Carlos!5e0!3m2!1spt-BR!2sbr!4v1733445699924!5m2!1spt-BR!2sbr" width="300" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div>
-                <h3>Sobre nós</h3>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
+                <h3>Conheça-nos</h3>
+                <p>Quem somos</p>
             </div>
             <div>
-                <h3>Entre em contato</h3>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
+                <h3>Ganhe dinheiro conosco</h3>
+                <p>Anuncie sua quadra</p>
+                <p>Seja um assosiado</p>
             </div>
             <div>
-                <h3>Entre em contato</h3>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
+                <h3>Vamos te ajudar</h3>
+                <p>Sua conta</p>
+                <p>Devoluções e reembolsos</p>
+                <p>Ajuda e Suporte</p>
             </div>
         </div>
 
