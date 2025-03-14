cfba02027c1788c43b9a21cc4a24b1d28968df72
Gabriel Dias <gabesarcal215@gmail.com>
2025-03-13 22:16:37 -0300

    Conectando no banco de dados

--- app/perfil/page.js
+++ app/perfil/page.js
@@ -1,24 +1,34 @@
 'use client'
 
-export default Perfil;
-import { useEffect, useState } from "react";
+export default Teste;
 
-function Perfil() {
+import axios from "axios";
+import { useState } from "react";
 
-    const [ usuario, alteraUsuario ] = useState({})
+function Teste() { // É preciso instalar o pacote "axios", ele quem vai fazer a ligação do Beckend e Frontend
 
-    useEffect(()=> {
-        const dados = JSON.parse( localStorage.getItem("usuario"));
-        alteraUsuario(dados);
-    }, [])
+    const [ dados, alteraDados] = useState("")
+
+    async function testar(){ // Toda função que demorar para receber uma resposta coloque "await" e "async", porque ai o site consegue carregar inteiro e depois essa função, caso contrario o site todo não carregar até todas as informações chegarem
+        const response = await axios.get("http://localhost:3001/usuario");
+        console.log(response);
+
+        alteraDados(response.data[0]);
+    }
 
     return ( 
         <main>
-
-            <h1>Perfil do usuário</h1>
-
-            <p>id: {usuario.id} </p>
-            <p>Email: {usuario.email}</p>
+            <div className="layout-container">
+                <div>
+                    
+                </div>
+                <h1>Teste</h1>
+                <button onClick={()=>testar()}>Testar a API</button>
+                <p>Resultado é: {dados.nome}</p>
+                <p>Resultado é: {dados.nascimento}</p>
+                <p>Resultado é: {dados.email}</p>
+                <p>Resultado é: {dados.cpf}</p>
+            </div>
         </main>
      );
 }
--- app/teste/page.js
+++ app/teste/page.js
@@ -3,12 +3,17 @@
 export default Teste;
 
 import axios from "axios";
+import { useState } from "react";
 
 function Teste() { // É preciso instalar o pacote "axios", ele quem vai fazer a ligação do Beckend e Frontend
 
+    const [ dados, alteraDados] = useState("")
+
     async function testar(){ // Toda função que demorar para receber uma resposta coloque "await" e "async", porque ai o site consegue carregar inteiro e depois essa função, caso contrario o site todo não carregar até todas as informações chegarem
-        const response = await axios.get("http://localhost:3001/mensagem");
+        const response = await axios.get("http://localhost:3001/usuario");
         console.log(response);
+
+        alteraDados(response.data[0]);
     }
 
     return ( 
@@ -16,6 +21,11 @@ function Teste() { // É preciso instalar o pacote "axios", ele quem vai fazer a
             <h1>Teste</h1>
 
             <button onClick={()=>testar()}>Testar a API</button>
+
+            <p>Resultado é: {dados.nome}</p>
+            <p>Resultado é: {dados.nascimento}</p>
+            <p>Resultado é: {dados.email}</p>
+            <p>Resultado é: {dados.cpf}</p>
         </div>
      );
 }
