e882bb33db0f42449b6cf0b78a683263dc9af21e
Gabriel Dias <gabesarcal215@gmail.com>
2025-03-21 21:38:43 -0300

    Criando página de Sobre Nós
    Modificando e alterando página de Local, criando um componente para ver as avaliações dos usuários

--- app/components/quadras.js
+++ app/components/quadras.js
@@ -6,9 +6,7 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 export default function Quadras(attr) {
     return (
        
-        <main className="border border-gray-300 shadow-lg w-80 py-2 px-1 m-2 rounded-lg hover:scale-105 transition-transform duration-300">
-            
-            <div>
+        <main className="w-80 py-2 px-2 m-2 rounded-lg border-solid border-gray-300 border"> 
                 <div className="flex justify-between items-center">
                     <p className="text-gray-700">⭐⭐⭐ {attr.avaliacoes}</p>
                     <FontAwesomeIcon icon={faHeart}
@@ -27,10 +25,9 @@ export default function Quadras(attr) {
                     <p className="text-sm h-[40px] text-gray-500">{attr.localizacao}</p>
                 </div>
 
-                <button className="w-5/6 bg-white rounded-sm hover:bg-gray-300 p-1"
+                <button className="cursor-pointer w-5/6 bg-white rounded-sm hover:bg-gray-300 p-1"
                         onClick={() => window.location.href = "local/" + attr.id} >Alugar
                 </button>
-            </div>
         </main>
 
       
---
+++ app/sobre/page.js
@@ -0,0 +1,26 @@
+export default Sobre;
+
+function Sobre() {
+    return ( 
+        <main>
+            <div className="layout-container">
+                <p>
+                    Sobre Nós
+                    No mundo do esporte e do lazer, encontrar a quadra perfeita para jogar com amigos, treinar ou realizar eventos esportivos pode ser um desafio. Pensando nisso, criamos [Nome do Seu Site], uma plataforma inovadora que conecta donos de quadras a esportistas e organizadores, tornando o processo de aluguel mais fácil, rápido e acessível.
+                </p>
+
+                ![Imagem representativa de pessoas jogando em uma quadra moderna]
+                
+                <p>
+                    Nosso objetivo é oferecer visibilidade para os proprietários de quadras, ajudando-os a preencher horários vagos e otimizar seus negócios, enquanto os usuários encontram os melhores espaços com apenas alguns cliques. Com um sistema intuitivo e prático, buscamos revolucionar a forma como quadras esportivas são alugadas.
+                </p>
+
+                ![Imagem de uma interface de reserva online em um celular ou computador]
+
+                <p>
+                    Seja você um dono de quadra buscando mais clientes ou um apaixonado por esportes à procura do espaço ideal, estamos aqui para facilitar sua jornada. Junte-se a nós e descubra uma nova maneira de conectar pessoas ao esporte!
+                </p>
+            </div>
+        </main>
+     );
+}
