3734e06e07a749216246c80e4307b02d03ef0753
Gabriel Dias <107761385+GabesArcal@users.noreply.github.com>
2025-05-23 20:40:15 -0300

    Mexendo no layout Menu e Rodapé

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -5,8 +5,6 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
- 
-
 export default function Menu() {
   const [user, alteraUser] = useState({});
 
@@ -23,56 +21,46 @@ export default function Menu() {
     } , [])
 
   return ( 
-    <main>
-      <div className="bg-green-500">
-        <div className="layout-container pb-1 mb-4">
-          <div className="flex justify-between">
-            <img src="/imagens/logo.jpg" onClick={() => window.location.href="/"} className="w-32" />
+    <main className="bg-green-500">
+      <nav className="layout-container flex">   
 
-            <div className="cursor-pointer items-center flex w-80">  
-              <div className="bg-white p-3 rounded-md ">
-                <input className="w-64 pb-2 outline-none border-none" placeholder="Digite o nome da quadra" />
-                <FontAwesomeIcon icon={faSearch} className="text-gray-500 text-2xl"/>
-              </div>
-            </div>
-          </div>    
-          
-          {/* Navegação */}
-          <nav>
-            <div>
-              <ul className="text-white flex justify-between list-none">
-                <li className="cursor-pointer hover:underline" onClick={() => window.location.href = "/categoria/"}>Todas as categorias</li>
-                <a
-                  href="https://wa.me/5511999999999"
-                  target="_blank"
-                  className="text-white cursor-pointer no-underline hover:underline">
-                    Anuncie sua quadra
-                </a>
-                <div className="flex">
-                  {
-                    user.id === undefined && (
-                    <li onClick={() => window.location.href = "/cadastro"} className="cursor-pointer hover:underline">
-                      Crie sua conta
-                    </li>
-                      )
-                      }
-                  {
-                    user.id === undefined ? (
-                    <li onClick={() => window.location.href = "/login"} className="cursor-pointer mx-10 hover:underline">
-                      Entre
-                    </li>
-                    ) : (
-                    <li onClick={() => window.location.href = "/perfil"} className="cursor-pointer mx-10 hover:underline">
-                    Perfil
-                    </li>
-                    )
-                  }
-                </div>
-              </ul>
-            </div>
-          </nav>  
-        </div>
-      </div> 
+        <img src="/imagens/logo.jpg" onClick={() => window.location.href="/"} className="w-32" />
+
+        <ul className="text-white justify-around flex w-screen items-end list-none">
+          <li className="cursor-pointer text-xl hover:underline" onClick={() => window.location.href = "/categoria/"}>
+            Todas as categorias
+          </li>
+
+          <a
+            href="https://wa.me/5511999999999"
+            target="_blank"
+            className="text-white text-xl cursor-pointer no-underline hover:underline">
+              Anuncie sua quadra
+          </a>
+
+          <div className="flex">
+            {
+              user.id === undefined && (
+              <li onClick={() => window.location.href = "/cadastro"} className="text-xl cursor-pointer hover:underline">
+                Crie sua conta
+              </li>
+                )
+            }
+
+            {
+                user.id === undefined ? (
+                <li onClick={() => window.location.href = "/login"} className="text-xl cursor-pointer mx-10 hover:underline">
+                  Entre
+                </li>
+                ) : (
+                <li onClick={() => window.location.href = "/perfil"} className="text-xl cursor-pointer mx-10 hover:underline">
+                Perfil
+                </li>
+                )
+              }
+          </div>
+        </ul>
+      </nav>  
     </main>
   );
 }
--- app/components/Rodape.js
+++ app/components/Rodape.js
@@ -7,13 +7,13 @@ function RodaPe() {
         <div className="bg-green-700">
             <footer className="text-white layout-container flex justify-between">
                 <div>
-                    <h3 className="font-bold mb-2">Conheça-nos</h3>
+                    <h3 className="font-bold mb-2 text-2xl">Conheça-nos</h3>
                     <p className="cursor-pointer hover:underline" onClick={() => window.location.href = "/sobre"}>
                         Sobre a PlayFute
                     </p>
                 </div>
                 <div>
-                    <h3 className="font-bold mb-2">Ganhe dinheiro conosco</h3>
+                    <h3 className="font-bold mb-2 text-2xl">Ganhe dinheiro conosco</h3>
                     <a
                         href="https://wa.me/5511999999999"
                         target="_blank"
@@ -22,7 +22,7 @@ function RodaPe() {
                     </a>
                 </div>
                 <div>
-                    <h3 className="font-bold mb-2 cursor-pointer">Vamos te ajudar</h3>
+                    <h3 className="font-bold mb-2 cursor-pointer text-2xl">Vamos te ajudar</h3>
                     <p className="cursor-pointer hover:underline cursor-pointer" onClick={() => window.location.href = "/perfil"}>
                         Sua conta
                     </p>
